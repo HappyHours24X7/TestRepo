c03f279bd14149cabf660615ac23fdc0edb99eb1
APARNA SAHA <88734994+HappyHours24X7@users.noreply.github.com>
2023-03-26 10:11:23 +0530

    Add files via upload
    Hello world Program

---
+++ First.java
@@ -0,0 +1,12 @@
+package DayOne;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			    System.out.println("Hello World");
+			  }
+			}
+
+
